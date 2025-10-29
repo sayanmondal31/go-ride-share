2713f8eb6dd95656e840dd007c589045d4760e32
drivadosayan <techsupport1@drivado.com>
2025-10-29 16:16:54 +0530

    go-ride-share

---
+++ services/trip-service/internal/service/service.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"context"
+	"ride-sharing/services/trip-service/internal/domain"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Service struct {
+	repo domain.TripRepository
+}
+
+func NewService(repo domain.TripRepository) *Service {
+	return &Service{
+		repo: repo,
+	}
+}
+
+func (s *Service) CreateTrip(ctx context.Context, fare *domain.RideFareModel) (*domain.TripModel, error) {
+
+	t := &domain.TripModel{
+		ID:       primitive.NewObjectID(),
+		UserID:   fare.UserID,
+		Status:   "Pending",
+		RideFare: fare,
+	}
+
+	return s.repo.CreateTrip(ctx, t)
+}
